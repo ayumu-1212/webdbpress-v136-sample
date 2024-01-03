ac5f923b1aea87e8372cd9bfd90936d2c2d9aa6c
ayumu-1212 <ayumuabe1434@gmail.com>
2024-01-03 13:07:53 +0000

    ✍️ 2章ひととおり実装

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ app = FastAPI()
 @app.get("/hello")
 def get_hello():
   return {"hello": "world"}
+
+@app.get("/wallets")
+def get_wallets() -> dict[str, int]:
+  return {"wallet_id": 1}
+
+@app.post("/wallets")
+def post_wallets() -> dict[str, int]:
+  return {"wallet_id": 2}
+
+@app.get("/wallets/meta")
+def get_wallet_meta() -> dict[str, dict[str, int]]:
+  return {"meta": {"count": 2}}
+
+@app.get("/wallets/{wallet_id}")
+def get_wallets_id(
+  wallet_id: int,
+) -> dict[str, int]:
+  return {"wallet_id": wallet_id}
