aabef1e604505542027a7e3471ca6b50c5eb91e6
ayumu-1212 <ayumuabe1434@gmail.com>
2024-01-03 14:11:25 +0000

    ✍️ schema追加

--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,20 @@
-from pydantic import BaseModel
+from enum import StrEnum
+from pydantic import BaseModel, PositiveInt
+
+class HistoryType(StrEnum):
+  INCOME = "INCOME"
+  OUTCOME = "OUTCOME"
+
+class History(BaseModel):
+  history_id: int
+  name: str
+  amount: PositiveInt
+  type: HistoryType
 
 class Wallet(BaseModel):
   wallet_id: int
   name: str
+  histories: list[History]
 
 class CreateWalletRequest(BaseModel):
   name: str
-  
